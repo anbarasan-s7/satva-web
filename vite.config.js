cc699c5125fb2f653ff17eef42c4caf2498f3e6e
Anbarasan <anbusaba2003@gmail.com>
2025-09-01 10:53:50 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
@@ -7,5 +7,5 @@ export default defineConfig({
   plugins: [react(),
            tailwindcss(),
   ],
-  base:"/satva-web/"
+  base:"/satva-web/",
 })
